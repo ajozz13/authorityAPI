df276d45a94037d0a7cdfba1ab3ea96cd5bc9951
Alberto Ochoa <ajozz13@gmail.com>
2018-01-06 12:09:54 -0500

    add CORS headers response

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ mongoose.connect( config.database_url );
 //secret
 app.set( 'SuperSecret', config.secret );
 
+//Accept cross-origin browser requests
+app.use( function( req, res, next ){
+  res.setHeader( "Access-Control-Allow-Methods", "HEAD, OPTIONS, DELETE, POST, GET" );
+  res.header( "Access-Control-Allow-Origin", "https://www.ibcinc.com" );
+  res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+  res.header( "Access-Control-Allow-Credentials", true );
+  next();
+});
+
 //body-parser dbSetup
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( bodyParser.json() );
